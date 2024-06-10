ae6f75f9bedf7787a96d9f8c0a42fb074d7cdd1d
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-06-10 04:49:30 -0700

    Loops in Array

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-var temii = ["jiii","jnad","billli","junaid","annna","haba"];
+// var temii = ["jiii","jnad","billli","junaid","annna","haba"];
 // console.log(temii.length);
 
-temii.forEach(getelement)
 
-function getelement(element , index){ if(element == jnad){ console.log(hi) } }
+var nameofEmploy = ["masood","mannan","hannan","hadi","amjad","ali"] ;
+
+// nameofEmploy.slice(0,2);
+// nameofEmploy.splice(2 , 0 ,"Ajjjjjj","kallllll","loooooooo");
+// nameofEmploy.unshift("ali aja ");
+// nameofEmploy.push("amjad" , "kamran" ,"sohail");
+console.log(nameofEmploy.length);
+console.log("original :" , nameofEmploy);
+
+
+function getCalled(value , index , arr) {
+    if (value == hannan){
+        console.log("failed", value);
+    }
+    else{
+        console.log("Passed" , value)
+    }
+}
+
+nameofEmploy.forEach(getCalled);

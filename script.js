e35deb6ad3922c585f40d966faaac897f40a3048
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-07-05 06:28:44 -0700

    LOOP IN ARRAY

--- script.js
+++ script.js
@@ -15,24 +15,56 @@
 
 // var nameofEmploys = [" aki","mona","sajiiid","farime","amna","sajid","junagt","hassan","jumah","ajnna","aki"]
 // console.log(nameofEmploy.length)
-var nameofEmploy = ["masood","mannan","hannan","hadi","amjad","ali"] ;
+// var nameofEmploy = ["masood","mannan","hannan","hadi","amjad","ali"] ;
 
-function getCalled(value , index ,arr){
-  if (value == "hannan" ){
-   return value + "failed"
-  }
-  else if(value == "mannan"){
-   return value + "Failed"
-  }
-  else if(value == "ali"){
-   return value + "failed"
-  }
-  else{
-   return value + "passed"
-  }
-} 
-var result = nameofEmploy.map(getCalled)
-console.log(result)
+// function getCalled(value , index ,arr){
+//   if (value == "hannan" ){
+//    return value + "failed"
+//   }
+//   else if(value == "mannan"){
+//    return value + "Failed"
+//   }
+//   else if(value == "ali"){
+//    return value + "failed"
+//   }
+//   else{
+//    return value + "passed"
+//   }
+// } 
+// var result = nameofEmploy.map(getCalled)
+// console.log(result)
 
 // nameofEmploys.map(getCalled);
 
+
+var codeofEmploys = [12 , 24, 6, 6, 93, 92,44, 42, 324, 235,22 ,325];
+console.log(codeofEmploys.length);
+console.log("original :",codeofEmploys);
+
+function called(value){
+if(value == 6){
+   return "OUT"
+}
+else{
+   return value
+}
+
+}
+
+var result = codeofEmploys.map(called);
+console.log(result);
+
+
+
+var max = ["ali","jamat","imran","kaleem","kubra"];
+function called(value , index , arr){
+   if  ( value == "ali"){
+      console.log("putter chuti kr");
+   }
+   else{
+      console.log(value)
+   }
+return value + "hi";
+}
+var result1 = max.map(called);
+console.log(result1)
